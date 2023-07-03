f526a433c9026405eab32efe07fb4a39350b17e8
koesnam <76582376+10kseok@users.noreply.github.com>
2023-07-03 17:23:31 +0900

    say bye api

---
+++ src/main/java/com/example/jenkinstest/ByeRestController.java
@@ -0,0 +1,25 @@
+package com.example.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * {설명을 작성해주세요}
+ *
+ * @author koesnam
+ * @since 2023/07/03
+ */
+
+@RestController
+public class ByeRestController {
+
+    @GetMapping
+    public String sayBye() {
+        return "Bye";
+    }
+
+    @GetMapping("v2")
+    public String sayBye2() {
+        return "good Bye, bro";
+    }
+}
